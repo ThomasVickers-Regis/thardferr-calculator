5a5de5b1bfe63903f80d161c19437f9a6f3da032
GTOVD <vickerst10@gmail.com>
2025-07-23 23:22:38 -0600

    next config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ignoreDuringBuilds, true,
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
